70705a5b774f5d811562cbc764b7a550f09e5087
boco0711 <s.lep.0711@gmail.com>
2020-03-08 22:48:02 +0100

    Commit initial

---
+++ app/src/main/java/com/cleanup/todoc/base/BaseActivity.java
@@ -0,0 +1,22 @@
+package com.cleanup.todoc.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Objects;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(this.getLayoutContentViewID());
+        ButterKnife.bind(this);
+    }
+
+    public abstract int getLayoutContentViewID();
+}
